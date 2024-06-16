4a62b7d6f7ab7059427b50e04f4e5bc9d54b1600
Roshan Kr Singh <155691968+Roshansingh9@users.noreply.github.com>
2024-06-16 18:57:43 +0530

    Time: 3 ms (83.44%), Space: 13.7 MB (82.46%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long miss = 1;
+        int i = 0;
+        int patches = 0;
+        
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                patches++;
+            }
+        }
+        
+        return patches;
+    }
+};
