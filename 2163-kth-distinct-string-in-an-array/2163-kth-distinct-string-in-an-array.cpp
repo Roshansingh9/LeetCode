c7b3c18a5030f7ad24f8358e8046702cb54437d6
Roshan Kr Singh <155691968+Roshansingh9@users.noreply.github.com>
2024-08-05 22:26:07 +0530

    Time: 11 ms (90.72%), Space: 20.3 MB (5.44%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
@@ -8,12 +8,12 @@ public:
     string kthDistinct(vector<string>& arr, int k) {
         unordered_map<string, int> frequency;
         
-        // Count frequencies of each string
+        
         for (const string& str : arr) {
             frequency[str]++;
         }
         
-        // Collect distinct strings
+        
         vector<string> distinctStrings;
         for (const string& str : arr) {
             if (frequency[str] == 1) {
@@ -21,7 +21,7 @@ public:
             }
         }
         
-        // Return the k-th distinct string if it exists
+       
         if (k <= distinctStrings.size()) {
             return distinctStrings[k - 1];
         } else {
