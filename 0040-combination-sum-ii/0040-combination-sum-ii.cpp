dc1c006dd6c9a17e8af9e4eaa76299cd14ad8f1f
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-07-30 16:58:11 +0530

    Time: 0 ms (100%), Space: 14 MB (60.95%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 private:
     void solution(vector<vector<int>>& ans, vector<int>& sub, vector<int>& nums,
-                  int target, int currsum,int index) {
-        if (currsum == target) {
+                  int target, int index) {
+        if (target == 0) {
             ans.push_back(sub);
-        } else if (currsum > target) {
             return;
         }
-    for (int i = index; i < nums.size(); i++) {
-        if (i > index && nums[i] == nums[i - 1])
-            continue;
-        sub.push_back(nums[i]);
-        solution(ans, sub, nums, target, currsum + nums[i],i+1);
-        sub.pop_back();
+        for (int i = index; i < nums.size(); i++) {
+            if (i > index && nums[i] == nums[i - 1])
+                continue;
+
+            if (nums[i] > target)
+                break;
+            sub.push_back(nums[i]);
+            solution(ans, sub, nums, target - nums[i], i+1 );
+            sub.pop_back();
+        }
     }
 
-}
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> sub;
+        sort(candidates.begin(), candidates.end());
+        solution(ans, sub, candidates, target, 0);
 
-public : vector<vector<int>>
-         combinationSum2(vector<int>& candidates, int target) {
-    vector<vector<int>> ans;
-    vector<int> sub;
-    sort(candidates.begin(), candidates.end());
-    solution(ans, sub, candidates, target, 0,0);
-    return ans;
-}
-}
-;
+        return ans;
+    }
+};
