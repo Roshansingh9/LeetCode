e2c53c59bf6a77714f8bb31d711885aa4ba62cb4
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-07-30 16:14:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (28.03%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,23 @@
 class Solution {
 private:
-  vector<vector<int>> generate(vector<vector<int>>& ans,vector<int>& subset,vector<int>& nums,int i,int size){
-     if (i == size) {
-            ans.push_back(subset);  
-            return ans;
+    void solution(vector<int>& nums, vector<vector<int>>& ans,
+                  vector<int>& subarray, int i, int size) {
+        if (i >= size) {
+            ans.push_back(subarray);
+            return;
         }
-    generate(ans,subset,nums,i+1,size);
-    subset.push_back(nums[i]);
-    generate(ans,subset,nums,i+1,size);
-    subset.pop_back();
-    return ans;
-  }
+        solution(nums, ans, subarray, i + 1, size);
+        subarray.push_back(nums[i]);
+        solution(nums, ans, subarray, i + 1, size);
+        subarray.pop_back();
+    }
+
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int size=nums.size();
-        vector<int>subset;
-        vector<vector<int>>ans;
-        return generate(ans,subset,nums,0,size);
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> subarray;
+        solution(nums, ans, subarray, 0, n);
+        return ans;
     }
 };
