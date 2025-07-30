1eea56bff639a7caffeb2d7de3744d77e87a2bbe
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-07-30 16:27:06 +0530

    Time: 3 ms (36.46%), Space: 14.6 MB (15.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
-private:
-    void solution(vector<vector<int>>& ans, vector<int> nums, vector<int>& sub,
-                  int ind) {
-        ans.push_back(sub);
-        for (int i = ind; i < nums.size(); i++) {
-            if (i > ind && nums[i] == nums[i-1])
-                continue;
-            sub.push_back(nums[i]);
-            solution(ans, nums, sub, i + 1);
-            sub.pop_back();
+    private:
+    void solution(set<vector<int>>& ans,vector<int>& nums,vector<int>& sub,int i,int size){
+        if(i>=size ){
+            ans.insert(sub);
+            return;
         }
+        solution(ans,nums,sub,i+1,size);
+        sub.push_back(nums[i]);
+        solution(ans,nums,sub,i+1,size);
+        sub.pop_back();
     }
-
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> sub;
-        solution(ans, nums, sub, 0);
-        return ans;
+        sort(nums.begin(),nums.end());
+       set<vector<int>>ans;
+       vector<int> sub;
+       solution(ans,nums,sub,0,nums.size());
+       return vector<vector<int>>(ans.begin(),ans.end());
     }
 };
