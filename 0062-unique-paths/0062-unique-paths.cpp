0cba29b3ce3bbfe99f097cf810a0bca2239c9a81
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-08-02 08:00:01 +0530

    Time: 2 ms (18.41%), Space: 9.4 MB (8.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,17 @@
 class Solution {
+    private:
+    int solution(int m,int n,vector<vector<int>>& dp){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0 ) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int left=solution(m,n-1,dp);
+        int right=solution(m-1,n,dp);
+        return dp[m][n]=left+right;
+    }
 public:
     int uniquePaths(int m, int n) {
-        int N=n+m-2;
-        int r=m-1;
-        double res=1;
-        for(int i=1;i<=r;i++){
-            res=res*(N-r+i)/i;
-        }
-        return (int)res;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return solution(m-1,n-1,dp);
     }
 };
