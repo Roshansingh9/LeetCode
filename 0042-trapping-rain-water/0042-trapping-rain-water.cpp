b7c812d4edb6b0ed41ca1eb542e73452e52e7e1b
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-08-11 11:04:49 +0530

    Time: 0 ms (100%), Space: 26 MB (60.12%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,28 @@
 class Solution {
-    private:
-    vector<int>prefixsum(vector<int>& heights){
-        int n=heights.size();
-        vector<int>result(n);
-        result[0]=heights[0];
-        for(int i=1;i<n;i++){
-            result[i]=max(heights[i],result[i-1]);
-        }
-        return result;
-    }
-    vector<int>suffixsum(vector<int>& heights){
-        int n=heights.size();
-        vector<int>result(n);
-        result[n-1]=heights[n-1];
-        for(int i=n-2;i>0;i--){
-            result[i]=max(heights[i],result[i+1]);
-        }
-        return result;
-    }
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>presum=prefixsum(height);
-        vector<int>sufsum=suffixsum(height);
-        int total=0;
-        for(int i=0;i<n;i++){
-            int maxleft=presum[i],maxright=sufsum[i];
-            if(maxleft>height[i] && maxright>height[i])
-            total+=(min(maxleft,maxright)-height[i]);
+        int n = height.size();
+        int left = 0;
+        int right = n - 1;
+        int res = 0;
+        int maxLeft = 0, maxright = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    res += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxright) {
+                    maxright = height[right];
+                } else {
+                    res += maxright - height[right];
+                }
+                right--;
+            }
         }
-        return total;
+        return res;
     }
 };
