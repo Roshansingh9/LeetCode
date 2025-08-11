4f0095d2708328040f6bf3ab34645d893127ea8b
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-08-11 10:54:43 +0530

    Time: 0 ms (100%), Space: 27.3 MB (19.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,26 +1,34 @@
 class Solution {
+    private:
+    vector<int>prefixsum(vector<int>& heights){
+        int n=heights.size();
+        vector<int>result(n);
+        result[0]=heights[0];
+        for(int i=1;i<n;i++){
+            result[i]=max(heights[i],result[i-1]);
+        }
+        return result;
+    }
+    vector<int>suffixsum(vector<int>& heights){
+        int n=heights.size();
+        vector<int>result(n);
+        result[n-1]=heights[n-1];
+        for(int i=n-2;i>0;i--){
+            result[i]=max(heights[i],result[i+1]);
+        }
+        return result;
+    }
 public:
     int trap(vector<int>& height) {
-        int left = 0, right = height.size() - 1;
-        int leftMax = 0, rightMax = 0;
-        int ans = 0;
-
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    ans += leftMax - height[left];
-                left++;
-            } else {
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    ans += rightMax - height[right];
-                right--;
-            }
+        int n=height.size();
+        vector<int>presum=prefixsum(height);
+        vector<int>sufsum=suffixsum(height);
+        int total=0;
+        for(int i=0;i<n;i++){
+            int maxleft=presum[i],maxright=sufsum[i];
+            if(maxleft>height[i] && maxright>height[i])
+            total+=(min(maxleft,maxright)-height[i]);
         }
-
-        return ans;
+        return total;
     }
 };
