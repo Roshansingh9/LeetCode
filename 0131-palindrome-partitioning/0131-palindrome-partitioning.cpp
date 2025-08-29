e8b3ccab33c9a42a8683fbeb06403184c5a89e50
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-08-30 00:14:14 +0530

    Time: 31 ms (88.35%), Space: 53.5 MB (85.64%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,36 +1,36 @@
 class Solution {
 private:
-    bool ispal(const string& s,int start,int end) {
-      
+    bool ispal(string& s, int start, int end) {
         while (start < end) {
-            if (s[start] != s[end])
+            if (s[start] != s[end]) {
                 return false;
+            }
             start++;
             end--;
         }
         return true;
     }
-
-    void solution(vector<vector<string>>& ans, vector<string>& sub, string& s, int index) {
-        if(index==s.size()){
-        ans.push_back(sub);
-        return;
+    void helper(vector<vector<string>>& ans, vector<string>& sub, string& s,
+                int index) {
+        if (index == s.size()) {
+            ans.push_back(sub);
+            return;
         }
-        for(int i=index;i<s.size();i++){
-            if(ispal(s,index,i)){
-                sub.push_back(s.substr(index,i-index+1));
-                solution(ans,sub,s,i+1);
+
+        for (int i = index; i < s.size(); i++) {
+            if (ispal(s, index, i)) {
+                sub.push_back(s.substr(index, i - index + 1));
+                helper(ans, sub, s, i + 1);
                 sub.pop_back();
             }
         }
-        
     }
 
 public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> sub;
-        solution(ans, sub, s, 0);
+        helper(ans, sub, s, 0);
         return ans;
     }
 };
