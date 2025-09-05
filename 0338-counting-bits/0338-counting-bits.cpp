02da87b9838f2ea3f57a319e5f58e34fe4d2fbe4
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-09-05 09:02:39 +0530

    Time: 3 ms (26.33%), Space: 11.7 MB (9.25%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,14 @@
 class Solution {
+    vector<int>dp;
     private:
     int one_bits(int n){
-       int ans=0;
-       while(n!=0){
-        n=n&(n-1);
-        ans++;
-       }
-       return ans;
+       if(n==0) return 0;
+       if(dp[n]!=-1) return dp[n];
+       return dp[n]=one_bits(n>>1)+(n&1);
     }
 public:
     vector<int> countBits(int n) {
+        dp=vector<int>(n+1,-1);
         vector<int>ans;
         for(int i=0;i<=n;i++){
             ans.push_back(one_bits(i));
