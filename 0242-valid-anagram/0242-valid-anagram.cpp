4248028495fb402f5f02c4b7f572b3a1b15197ae
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-09-06 10:51:19 +0530

    Time: 5 ms (29.93%), Space: 9.5 MB (98.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,9 +5,7 @@ public:
         if(l1!=l2) return false;
        sort(s.begin(),s.end());
        sort(t.begin(),t.end()); 
-       for(int i=0;i<l1;i++){
-        if(s[i]!=t[i]) return false;
-       }
-       return true;
+       
+       return s==t;
     }
 };
