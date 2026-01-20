994c7f335b1ca0ce3ec85155703f86591aea6591
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2026-01-20 14:11:12 +0530

    Time: 4 ms (6.24%), Space: 18.5 MB (20.87%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        int initial_color = image[sr][sc];
-        if (image[sr][sc] == color)
-            return image;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
+        
+        int startColor = image[sr][sc];
+        if (startColor == color) return image;
 
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         queue<pair<int, int>> q;
+
         q.push({sr, sc});
+        vis[sr][sc] = 1;
         image[sr][sc] = color;
+
+        int dr[] = {0, -1, 0, 1};
+        int dc[] = {-1, 0, 1, 0};
+
         while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
+            auto [r, c] = q.front();
             q.pop();
+
             for (int i = 0; i < 4; i++) {
-                int nr = r + drow[i];
-                int nc = c + dcol[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
                 if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    image[nr][nc] == initial_color) {
+                    !vis[nr][nc] && image[nr][nc] == startColor) {
+
+                    vis[nr][nc] = 1;
                     image[nr][nc] = color;
                     q.push({nr, nc});
                 }
             }
         }
+
         return image;
     }
 };
