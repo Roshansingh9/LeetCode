7b8855daeead5c699a6c40e9839cd60b2e2146d5
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2026-01-21 16:55:48 +0530

    Time: 0 ms (100%), Space: 15.1 MB (26.07%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,51 +1,64 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
+        if (board.empty()) return;
+
         int n = board.size();
         int m = board[0].size();
+
         vector<vector<int>> vis(n, vector<int>(m, 0));
         queue<pair<int, int>> q;
+
+    
         for (int i = 0; i < m; i++) {
             if (board[0][i] == 'O') {
-                q.push({0, i});
                 vis[0][i] = 1;
+                q.push({0, i});
             }
-            if (board[n - 1][i] == 'O') {
-                q.push({n - 1, i});
+            if (board[n - 1][i] == 'O') {   
                 vis[n - 1][i] = 1;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') {
-                q.push({i, 0});
-                vis[i][0] = 1;
-            }
-            if (board[i][m - 1] == 'O') {
-                q.push({i, m - 1});
-                vis[i][m - 1] = 1;
+                q.push({n - 1, i});
             }
         }
 
+      
+        for (int i = 1; i < n - 1; i++) {
+            if (board[i][0] == 'O') {
+                vis[i][0] = 1;
+                q.push({i, 0});
+            }
+            if (board[i][m - 1] == 'O') {
+                vis[i][m - 1] = 1;
+                q.push({i, m - 1});
+            }
+        }
+
+        int dr[] = {0, 1, 0, -1};
+        int dc[] = {-1, 0, 1, 0};
+
+       
         while (!q.empty()) {
             int r = q.front().first;
             int c = q.front().second;
-            q.pop();
-            int dx[] = {-1, 0, +1, 0};
-            int dy[] = {0, -1, 0, +1};
+            q.pop();  
+
             for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
                 if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    board[nr][nc] == 'O' && !vis[nr][nc]) {
+                    !vis[nr][nc] && board[nr][nc] == 'O') {
+
                     vis[nr][nc] = 1;
                     q.push({nr, nc});
                 }
             }
         }
 
+       
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O' && !vis[i][j]) {
+                if (!vis[i][j] && board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
             }
